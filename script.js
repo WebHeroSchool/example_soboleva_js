6acca69e6d08b073c00ffd56a1a47a5d5ca42128
Maria Soboleva <soboleva.mar89@gmail.com>
2021-05-05 13:24:00 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,112 @@
+let section = document.getElementById('container');
+let title = document.getElementById('header');
+let difficulty = document.getElementsByClassName('level__start');
+let card, choice, line;
+for (let e of difficulty) {
+  e.addEventListener('click', levelDifficulty => {
+    if (choice) {
+      choice.classList.toggle('choicen__start');
+    };
+    choice = levelDifficulty.target;
+    choice.classList.toggle('choicen__start');
+  });
+}
+
+let btn = document.getElementById('button');
+btn.addEventListener('click', start => {
+  choice.classList.toggle('choicen__start');
+goAndAgain();
+document.body.classList.toggle('game-page');
+(choice.id === 'easy') ? getEasy():
+(choice.id === 'normal') ? getNormal():
+getHard();
+choice = null;
+let cards = document.querySelectorAll('.game-page__img');
+let cardChoice;
+for (let e of cards) {
+  e.addEventListener('click', check => {
+    cardChoice = check.target;
+    let rotate = cardChoice.closest('.game-page__img');
+    rotate.classList.toggle('game-page__img_rotation');
+    for (let g of cards) {
+      g.addEventListener('click', getNewGame => {
+            let newGame = document.querySelectorAll('div');
+            for (let k of newGame) {
+             k.remove();
+            }
+            document.body.classList.toggle('game-page');
+            goAndAgain();
+          })
+    }
+  });
+ }
+});
+
+let createLine = () => {
+  line = document.createElement('div');
+  document.body.append(line);
+  line.classList.toggle('game-page__line');
+}
+
+let gameCards = () => {
+  card = document.createElement('div');
+  line.append(card);
+  card.classList.toggle('game-page__img');
+  getBackCardImg(getBackCard);
+  getFrontCardImg(getFrontCard);
+}
+
+let getBackCard = () => {
+  let back = document.createElement('div');
+  back.classlist.toggle('game-page__back');
+  card.append(back);
+  return back;
+}
+
+let getBackCardImg = () => {
+  let backImg = document.createElement('img');
+  backImg.src = 'img/back.png';
+  getBackCard().append(backImg);
+  backImg.classlist.toggle('card__pic');
+  return backImg;
+}
+
+let getFrontCard = () => {
+  let front = document.createElement('div');
+  card.append(front);
+  front.classList.toggle('game-page__front');
+  return front;
+}
+
+let getFrontCardImg = () => {
+  let frontImg = document.createElement ('img');
+  let cardRezult = getRandomInt(2);
+  (cardRezult === 0) ? frontImg.src = 'img/gameOver.png':
+  frontImg.scr = 'img/cardBug.png';
+  getFrontCard().append(frontImg);
+  frontImg.classList.toggle('card__rezult');
+  return frontImg;
+}
+
+let getEasy = () => {
+  if (window.innerWidth <= 375) {
+    for (let k = 0; k < 3; k++) {
+      createLine();
+      gameCards();
+  }
+  } else {
+    createLine();
+    for (let i = 0; i < 3; i++) {
+      gameCards();
+    }
+  }
+};
+
+let getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+let goAndAgain = () => {
+  title.classList.toggle('choice_then');
+  container.classList.toggle('choice_then');
+}
